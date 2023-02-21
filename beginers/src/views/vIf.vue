33764ad0b3bb609b6084f06c0cb7de87f78d4b3a
SanjarbekUsmonov <noobpusk@gmail.com>
2023-02-22 02:02:36 +0500

    v-if v-show

--- beginers/src/views/vIf.vue
+++ beginers/src/views/vIf.vue
@@ -1,4 +1,64 @@
 <template>
+  <div class="bir">
+    <button @click="showText = !showText">Click me</button>
+
+    <div v-if="showText">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eveniet
+      corporis ducimus voluptas fugiat culpa sed eaque, rem, ex, quo neque sint
+      porro? Dolorem dolorum fugit dignissimos tenetur et cum!
+    </div>
+
+    <div v-else>
+      hello ipsum dolor sit amet consectetur adipisicing elit. Saepe eveniet
+      corporis ducimus voluptas fugiat culpa sed eaque, rem, ex, quo neque sint
+      porro? Dolorem dolorum fugit dignissimos tenetur et cum!
+    </div>
+
+    <div v-show="showText">
+      Sanjar ipsum dolor sit amet consectetur adipisicing elit. Ipsam animi
+      provident, fugiat harum quam assumenda debitis hic omnis dicta nobis
+      voluptas. Repellat blanditiis, laboriosam magni optio dicta laudantium
+      accusantium debitis.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showText: true,
+    };
+  },
+};
+</script>
+
+<style  scoped>
+.bir {
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <template>
   <div>
     <div>
       <h1>Hello v-if</h1>
@@ -26,11 +86,11 @@ export default {
   },
   methods: {
     delateBtn(index) {
-      this.lorem.splice(index);
+      this.lorems.splice(index);
     },
   },
 };
 </script>
 
 <style scoped>
-</style>
+</style> -->
