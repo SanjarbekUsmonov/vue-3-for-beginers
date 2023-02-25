8ca239cf702d31dfaec436cfef6655bcd4aa6bf3
SanjarbekUsmonov <noobpusk@gmail.com>
2023-02-25 14:19:40 +0500

    tutorial

--- beginers/src/views/Takrorlash.vue
+++ beginers/src/views/Takrorlash.vue
@@ -2,7 +2,66 @@
     <div>
         <h1>Hello Takrorlash</h1>
         <div>
+            <input type="text" 
+            v-bind:placeholder="inputValue"
+            v-model="inputModel" 
+            >
+           <ul>
+             <li v-for="(array,index) in newArray" :key="index">{{ array }}
+             <button>x</button>
+            </li>
+         </ul>
+        </div>
+    </div>
+</template>
 
+<script>
+    export default {
+        data() {
+            return {
+                inputValue:"Bu yerga yozing",
+                inputModel:"",
+                newArray:["bir","ikki","uch","tort","besh"]
+            }
+        },
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <template>
+    <div>
+        <h1>Hello Takrorlash</h1>
+        <div>
+            <h1>{{schochik}}</h1>
+            <button @click="oldinga()">oldinga</button>
+            <button @click="orqaga()">orqaga</button>
         </div>
     </div>
 </template>
@@ -11,12 +70,24 @@
  export default {
      data() {
         return {
-            
+          schochik: 0  
         }
-     },   
+     },  
+     methods: {
+        oldinga(){
+           if(this.schochik < 10){
+            this.schochik++ 
+           }
+        },
+        orqaga(){
+           if(this.schochik > 0){
+            this.schochik--
+           }
+        },
+     }, 
 }
 </script>
 
 <style  scoped>
 
-</style>
+</style> -->
