03907b07fb2f546f79719f9db4895c486919ad94
SanjarbekUsmonov <noobpusk@gmail.com>
2023-02-25 14:38:07 +0500

    tutorial

--- beginers/src/views/Takrorlash.vue
+++ beginers/src/views/Takrorlash.vue
@@ -1,51 +1,52 @@
 <template>
+  <div>
+    <h1>Hello Takrorlash</h1>
     <div>
-        <h1>Hello Takrorlash</h1>
-        <div>
-            <input type="text" 
-            v-bind:placeholder="inputValue"
-            v-model="inputModel"
-            @keypress.enter="keypresInput()" 
-            >
-           <ul v-if="newArray.length !== 0">
-             <li v-for="(array,index) in newArray" :key="index">{{ array }}
-             <button @click="clickBtn(index)">x</button>
-            </li>
-         </ul>
-         <div v-else>Hech narsa qolmadi</div>
-         {{ numberArray }}
-        </div>
+      <input
+        type="text"
+        v-bind:placeholder="inputValue"
+        v-model="inputModel"
+        @keypress.enter="keypresInput()"
+      />
+      <ul v-if="newArray.length !== 0">
+        <li v-for="(array, index) in newArray" :key="index">
+          {{ array }}
+          <button @click="clickBtn(index)">x</button>
+        </li>
+      </ul>
+      <div v-else>Hech narsa qolmadi</div>
+      {{ numberArray }}
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                inputValue:"Bu yerga yozing",
-                inputModel:"",
-                newArray:["bir","ikki","uch","tort","besh"]
-            }
-        },
-        methods: {
-            clickBtn(index){
-                this.newArray.splice(index,1)
-            },
-            keypresInput(){
-                this.newArray.push(this.inputModel)
-                this.inputModel = ""
-            }
-        },
-        computed:{
-           numberArray(){
-             return this.newArray.length * 5
-           }
-        }
-    }
+export default {
+  data() {
+    return {
+      inputValue: "Bu yerga yozing",
+      inputModel: "",
+      newArray: ["bir", "ikki", "uch", "tort", "besh"],
+    };
+  },
+  methods: {
+    clickBtn(index) {
+      this.newArray.splice(index, 1);
+    },
+    keypresInput() {
+      this.newArray.push(this.inputModel);
+      this.inputModel = "";
+    },
+  },
+  computed: {
+    numberArray() {
+      return this.newArray.length * 5;
+    },
+  },
+};
 </script>
 
 <style lang="css" scoped>
-
 </style>
 
 
